c7ae4c19dcd10218d9d4c83b720895f2b71ce335
Navjot Singh <navjotd139@gmail.com>
2019-06-14 16:33:34 +0530

    java file for time fragment

---
+++ app/src/main/java/com/example/dialogbox/TimeFragment.java
@@ -0,0 +1,43 @@
+package com.example.dialogbox;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+
+public class TimeFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+
+
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            SecondActivity activity = (SecondActivity) getActivity();
+            activity.processTimePickerResult(hourOfDay,minute);
+    }
+}
