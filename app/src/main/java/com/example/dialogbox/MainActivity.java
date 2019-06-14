08c7cceaa58491c85ce1ae0163cee0b54ac76921
Navjot Singh <navjotd139@gmail.com>
2019-06-14 14:35:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dialogbox/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dialogbox;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showAlert(View view) {
+        AlertDialog.Builder myalertBuilder = new AlertDialog.Builder(MainActivity.this);
+        myalertBuilder.setTitle("Alert");
+        myalertBuilder.setMessage("Click OK to continue, or Cancel to stop:");
+
+        myalertBuilder.setPositiveButton("OK", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Pressed OK",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myalertBuilder.setNegativeButton("Cancel", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myalertBuilder.show();
+    }
+
+    public void launch(View view) {
+        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
