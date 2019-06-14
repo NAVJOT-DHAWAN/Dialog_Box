8f3c6472bb2d7ee19281b394f5a7f048deed8994
Navjot Singh <navjotd139@gmail.com>
2019-06-14 15:26:27 +0530

    date converted to string

--- app/src/main/java/com/example/dialogbox/SecondActivity.java
+++ app/src/main/java/com/example/dialogbox/SecondActivity.java
@@ -1,7 +1,14 @@
 package com.example.dialogbox;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+
+    public void showdateAlert(View view) {
+        DialogFragment newFragment = new BlankFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this, "Date: " + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
